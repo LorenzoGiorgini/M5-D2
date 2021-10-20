18ade47518995a71496bb9f13a6be2ee9d848c41
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-10-20 21:44:58 +0200

    finished backend part

--- src/services/blog/validation.js
+++ src/services/blog/validation.js
@@ -4,5 +4,5 @@ import { body } from "express-validator"
 export const blogValidation = [
   body("category").exists().withMessage("category is a mandatory field!"),
   body("title").exists().withMessage("title is a mandatory field!"),
-  body("cover").exists().withMessage("Cover is a mandatory field!")
+  body("content").exists().withMessage("content is a mandatory field!")
 ]
